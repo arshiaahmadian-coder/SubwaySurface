2b66ce07ecb9baf6d04867d029a1fae9eb8e638c
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2026-07-09 22:37:12 +0330

    importing envirement assets, player animations

--- Subway Surface/Assets/Scripts/Envirement/ChunkMove.cs
+++ Subway Surface/Assets/Scripts/Envirement/ChunkMove.cs
@@ -4,12 +4,13 @@ public class ChunkMove : MonoBehaviour
 {
     [SerializeField] GameObject chunkPrefab;
     [SerializeField] float spawnOffsed;
+    [SerializeField] float despawnPos;
 
     private void Update()
     {
         transform.Translate(0, 0, ChunkManager.singleton.chunkMoveSpeed * Time.deltaTime);
 
-        if (transform.position.z <= -50)
+        if (transform.position.z <= despawnPos)
         {
             // spawn another chunk
             Vector3 spawnPosition = new Vector3(0, 0, transform.position.z + spawnOffsed);
